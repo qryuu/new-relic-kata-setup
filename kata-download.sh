e18b02918e8cd98435a1f1621abff0b6adf7ba7d
qryuu <qryuu.maoto@gmail.com>
2020-02-18 14:23:37 +0900

    set sudo to initdb

--- kata-download.sh
+++ kata-download.sh
@@ -3,7 +3,7 @@
 sudo yum install -y postgresql postgresql-devel postgresql-server postgresql-contrib postgresql-docs
 
 sudo chkconfig postgresql on
-service postgresql initdb --encoding=UTF8 --no-locale
+sudo service postgresql initdb --encoding=UTF8 --no-locale
 sudo service postgresql start
 
 git clone https://github.com/newrelic/newrelic-ruby-kata 
