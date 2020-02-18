498d36a33d967795f63c8847f287d1b1dd4af656
qryuu <qryuu.maoto@gmail.com>
2020-02-18 15:52:39 +0900

    postgresql 設定変更

--- kata-download.sh
+++ kata-download.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 sudo yum install -y postgresql postgresql-devel postgresql-server postgresql-contrib postgresql-docs
-
+sudo sed -i -e "s/host    all             all             127.0.0.1\/32            ident/host    all             all             127.0.0.1\/32            trust/g" /var/lib/pgsql9/data/pg_hba.conf
 sudo chkconfig postgresql on
 sudo service postgresql initdb --encoding=UTF8 --no-locale
 sudo service postgresql start
 
+sudo -u postgres createuser -s ec2-user
+sudo -u postgres createdb ec2-user
+sudo -u postgres psql -c "ALTER USER \"ec2-user\" WITH SUPERUSER;"  
+
+sudo service postgresql restart
 git clone https://github.com/newrelic/newrelic-ruby-kata /home/ec2-user/environment/newrelic-ruby-kata
--- kata-install.sh
+++ kata-install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 cd /home/ec2-user/environment/newrelic-ruby-kata
+
 rvm rvmrc warning ignore allGemfiles
 rvm install "ruby-2.2.2"
+
+bundle install
+
+bundle exec rake db:create
+pg_restore --verbose --clean --no-acl --no-owner -h localhost -U $USER -d newrelic-ruby-kata_development public/sample-data.dump
+bundle exec rails s
