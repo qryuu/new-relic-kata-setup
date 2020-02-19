352c45c203afe7e84c87bbaf93c15f1992e17eec
qryuu <qryuu.maoto@gmail.com>
2020-02-19 12:25:05 +0900

    rvm インストールProcessを追加

--- kata-install.sh
+++ kata-install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
+curl -sSL https://rvm.io/pkuczynski.asc | gpg2 --import -
+curl -sSL https://get.rvm.io | bash -s stable
+source /home/ec2-user/.rvm/scripts/rvm
+
 cd /home/ec2-user/newrelic-ruby-kata
 
 rvm rvmrc warning ignore allGemfiles
