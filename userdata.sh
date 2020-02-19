2afb7fef9ee26ca087d2a8d69d5d424d99c82a64
qryuu <qryuu.maoto@gmail.com>
2020-02-19 11:50:37 +0900

    pg_hba.conf の編集順序を変更

--- userdata.sh
+++ userdata.sh
@@ -12,15 +12,14 @@ chmod 755 /home/ec2-user
 
 sudo -u ec2-user curl -L https://raw.githubusercontent.com/c9/install/master/install.sh | bash
 
-sed -i -e "s/host    all             all             127.0.0.1\/32            ident/host    all             all             127.0.0.1\/32            trust/g" /var/lib/pgsql/data/pg_hba.conf
-
 systemctl enable postgresql.service
 postgresql-setup initdb
 systemctl start postgresql.service
 
-sudo -u postgres createuser -s ec2-user
 sudo -u postgres createdb ec2-user
 sudo -u postgres psql -c "ALTER USER \"ec2-user\" WITH SUPERUSER;"  
+
+sed -i -e "s/host    all             all             127.0.0.1\/32            ident/host    all             all             127.0.0.1\/32            trust/g" /var/lib/pgsql/data/pg_hba.conf
 
 systemctl restart postgresql.service
 
